53e8f7c845279966ae4a9f378bc5ecc862b0e5fa
Django.Durano <anonymousab@mailbox.org>
2024-02-25 16:20:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
+echo "--- Checking python3 version ---"
 python=$(python3 --version 2>&1)
 
 if [[ $python == *"No such file or directory"* ]] || [[ $python == *"python3: command not found"* ]]; then
-  read -r -p "No python version found. Python is needed for working. Nerdtools python will not work.
-              Choose one to install: \n 1. Python 3.9.18 (own build python by me)\n2. Python 3.12.1 (by kubed_zero)" version
+  read -rep "No python version found. Python is needed for working. Nerdtools python will not work.\n
+  Choose one to install: \n1. Python 3.9.18 (own build python by me)\n2. Python 3.12.1 (by kubed_zero)" version
 
   if [[ $version == 1 ]]; then
     cd Python_Package || exit
-#    upgradepkg --install-new "python3-3.9.18-x86_64-1.txz"
+    upgradepkg --install-new "python3-3.9.18-x86_64-1.txz"
   else
     cd Python_Package || exit
-#    upgradepkg --install-new "python3-3.12.1-x86_64-1-kubed20231210.txz"
+    upgradepkg --install-new "python3-3.12.1-x86_64-1-kubed20231210.txz"
   fi
+  echo $PWD
 else
   echo $PWD
   python_test=$(python3 test_python.py 2>&1)
