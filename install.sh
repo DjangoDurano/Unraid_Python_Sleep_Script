ce834dfedeb048d61ea0e368cd5794b81412c75c
Django.Durano <anonymousab@mailbox.org>
2024-02-25 16:11:33 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,15 +7,14 @@ if [[ $python == *"No such file or directory"* ]] || [[ $python == *"python3: co
               Choose one to install: \n 1. Python 3.9.18 (own build python by me)\n2. Python 3.12.1 (by kubed_zero)" version
 
   if [[ $version == 1 ]]; then
-    cd /Unraid_Python_Sleep_Script/Python_Package || exit
+    cd Python_Package || exit
 #    upgradepkg --install-new "python3-3.9.18-x86_64-1.txz"
   else
-    cd /Unraid_Python_Sleep_Script/Python_Package || exit
+    cd Python_Package || exit
 #    upgradepkg --install-new "python3-3.12.1-x86_64-1-kubed20231210.txz"
   fi
 else
   echo $PWD
-  cd /Unraid_Python_Sleep_Script || exit
   python_test=$(python3 test_python.py 2>&1)
 
   if [[ $python_test == "Not worked!" ]]; then
